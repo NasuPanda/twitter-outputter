917037c1a2533c6f613e611fdebe3c0f3bfc7d03
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-15 16:15:27 +0900

    feat: Userのバリデーションのテストを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,18 @@
 class User < ApplicationRecord
+  validates :uid, presence: true, uniqueness: true
+  validate :provider_should_be_twitter
+
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
     uid = auth_hash[:uid]
-    # TODO ログイン中のユーザーの情報を追加する
-    # nickname = auth_hash[:info][:nickname]
 
     User.find_or_create_by!(provider: provider, uid: uid)
-    # User.find_or_create_by!(provider: provider, uid: uid) do |user|
-    #   user.name = nickname
-    # end
   end
+
+  private
+    def provider_should_be_twitter
+      if provider != 'twitter'
+        errors.add(:provider, "Twitter以外の値は無効です")
+      end
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーションのテスト" do
+    let(:user) { FactoryBot.create(:user) }
+    context "有効なユーザのとき" do
+      it "バリデーションに成功すること" do
+        expect(user).to be_valid
+      end
+    end
+    context "無効なユーザのとき" do
+      it "providerが無効な値であればバリデーションに失敗すること" do
+        user.provider = "invalid"
+        expect(user).to_not be_valid
+      end
+      it "uidが空であればバリデーションに失敗すること" do
+        user.uid = ""
+        expect(user).to_not be_valid
+      end
+    end
+  end
 end
