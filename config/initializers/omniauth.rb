27e2ac25974bb6d4d8db595f64a14813c1e45a3e
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-15 10:58:24 +0900

    fix: callbackURLが本番環境でエラーになる問題の修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   else
 		# 本番環境の設定: 秘密の文字列はcredentials.ymlから持ってくる
     provider :twitter,
-      Rails.application.credentials.twitter[:client_id],
-      Rails.application.credentials.twitter[:client_secret]
+      # Rails.application.credentials.twitter[:client_id],
+      # Rails.application.credentials.twitter[:client_secret]
+      "jC64rUrfYQzCypKLDCxPCTc26",
+      "EUmsEkHzm5tzl754lhKmAV3HUPn43EpNc72AQPnFQKaUEkLboQ"
   end
 end
