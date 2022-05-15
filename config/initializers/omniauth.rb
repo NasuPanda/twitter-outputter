1f57e429766bfae3120a27bc690d33416d81d666
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-15 11:14:34 +0900

    fix: credentialsを使用するように変更

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,9 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   else
 		# 本番環境の設定: 秘密の文字列はcredentials.ymlから持ってくる
     provider :twitter,
-      # Rails.application.credentials.twitter[:client_id],
-      # Rails.application.credentials.twitter[:client_secret]
-      "jC64rUrfYQzCypKLDCxPCTc26",
-      "EUmsEkHzm5tzl754lhKmAV3HUPn43EpNc72AQPnFQKaUEkLboQ"
+      Rails.application.credentials.twitter[:client_id],
+      Rails.application.credentials.twitter[:client_secret]
   end
 end
