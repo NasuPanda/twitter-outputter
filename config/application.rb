f9735e153581557dc4a217d6b02f24bb95ddf74d
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-18 07:32:02 +0900

    public_file_server.enabledのアクティブ化

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,7 @@ module TwitterOutputter
 
     # Herokuエラー対策
     config.assets.initialize_on_precompile = false
+    # publicディレクトリに置かれたあたゆるアセットは静的ファイルとして扱われる
+    config.public_file_server.enabled = true
   end
 end
