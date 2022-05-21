1b96a8c7fff85e0d0eb483ccd5b385a6fa0374be
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-21 20:00:32 +0900

    refactor: 暗号/復号化処理をauthenticationモデルに統合

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,15 +1,32 @@
 class Authentication < ApplicationRecord
+  scope :uid_is, -> uid {
+    where(uid: uid)
+  }
   before_save :encrypt_access_token
   belongs_to :user
 
   validates :uid, presence: true
 
-  # TODO cryptが復号側と共通なので、まとめたい
   def encrypt_access_token
-    key_len = ActiveSupport::MessageEncryptor.key_len
-    secret = Rails.application.key_generator.generate_key('salt', key_len)
-    crypt = ActiveSupport::MessageEncryptor.new(secret)
     self.access_token = crypt.encrypt_and_sign(access_token)
     self.access_token_secret = crypt.encrypt_and_sign(access_token_secret)
+  end
+
+  def decrypted_access_token
+    crypt.decrypt_and_verify(self.access_token)
+  end
+
+  def decrypted_access_token_secret
+    crypt.decrypt_and_verify(self.access_token_secret)
+  end
+
+  def crypt
+    @crypt ||= set_crypt
+  end
+
+  def set_crypt
+    key_len = ActiveSupport::MessageEncryptor.key_len
+    secret = Rails.application.key_generator.generate_key('salt', key_len)
+    ActiveSupport::MessageEncryptor.new(secret)
   end
 end
