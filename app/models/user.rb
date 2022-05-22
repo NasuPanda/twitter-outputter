6797d975e869caf1ad38f0811f7bd6b15fad131e
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-22 23:44:46 +0900

    refactor: SessionsControllerに書いていた処理をUserモデルに切り出し

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,50 +1,46 @@
-require 'bcrypt'
-
 class User < ApplicationRecord
+  scope :find_authentication_uid, -> (uid) {
+    joins(:authentication).merge(Authentication.uid_is(uid))
+  }
   has_one :authentication, dependent: :destroy
   accepts_nested_attributes_for :authentication
 
-  # ユーザの作成 or 取得
-  def create_authentication_from_auth_hash!(auth_hash)
-    self.build_authentication(
-      uid: auth_hash[:uid],
-      access_token: auth_hash.credentials.token,
-      access_token_secret: auth_hash.credentials.secret
-    )
-    self.save!
-  end
-
-  def set_external_user_id(external_user_id)
+  def find_or_create_external_user_id
+    return self.external_user_id if self.external_user_id
+    external_user_id = SecureRandom.uuid
     self.update_attribute(:external_user_id, external_user_id)
+    return external_user_id
   end
 
-  # TODO Twitter関連の処理をモジュールとして切り出す
-  # サービスオブジェクト/concernのどちらが最適化はまた考える。
-  # サービスオブジェクトは状態を持たないもの。callメソッドだけを持つ。複雑なドメインを分離するために使う。複数のモデルから使われるような場合使う。
   def post_tweet(text)
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = Rails.application.credentials.twitter[:client_id]
-      config.consumer_secret     = Rails.application.credentials.twitter[:client_secret]
-      config.access_token        = access_token
-      config.access_token_secret = access_token_secret
-    end
+    client = twitter_client
     client.update(text)
   end
 
-  def access_token
-    # self = user
-    crypt.decrypt_and_verify(self.authentication.access_token)
+  def self.find_or_create_by_auth_hash(auth_hash)
+    uid = auth_hash[:uid]
+    # scopeの返り値はRelationなので取り出す必要がある
+    unless user = self.find_authentication_uid(uid).first
+      user = self.new()
+      user.build_authentication(
+        uid: uid,
+        access_token: auth_hash.credentials.token,
+        access_token_secret: auth_hash.credentials.secret
+      )
+      user.save
+    end
+    return user
   end
 
-  def access_token_secret
-    # self = user
-    crypt.decrypt_and_verify(self.authentication.access_token_secret)
-  end
-
-  # TODO cryptが共通なので、まとめたい
-  def crypt
-    key_len = ActiveSupport::MessageEncryptor.key_len
-    secret = Rails.application.key_generator.generate_key('salt', key_len)
-    ActiveSupport::MessageEncryptor.new(secret)
-  end
+  private
+    # TODO Twitter関連の処理をモジュールとして切り出す。サービスオブジェクト/concernのどちらが最適かはまた考える。
+    # モックでテストしやすいようにメソッドに分離する
+    def twitter_client
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = Rails.application.credentials.twitter[:client_id]
+        config.consumer_secret     = Rails.application.credentials.twitter[:client_secret]
+        config.access_token        = self.authentication.decrypted_access_token
+        config.access_token_secret = self.authentication.decrypted_access_token_secret
+      end
+    end
 end
