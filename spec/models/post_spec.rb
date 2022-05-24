a920fc4950ae27602b85b648b52347610674ec5d
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-24 20:57:19 +0900

    refactor: Tag.nameのバリデーションにlengthを追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,54 +1,84 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  # validates :is_posted, presence: true, inclusion: [true, false]
-  # validates :post_at, presence: true, allow_nil: true
-  # validates :content, presence: true, length: { maximum: 140 }
-  describe "バリデーションのテスト" do
-    let(:post) { FactoryBot.create(:post) }
-    let(:draft) { FactoryBot.create(:post, :draft) }
+  let(:post) { FactoryBot.build(:post) }
+  let(:draft) { FactoryBot.build(:post, :draft) }
 
-    context "有効な属性のとき" do
+  describe 'attribute: is_posted' do
+    context '真偽値のとき' do
       it 'バリデーションに成功すること' do
-        expect(post).to be_valid
-      end
-
-      it '下書きかつpost_atが存在しないときバリデーションに成功すること' do
-        draft.post_at = nil
         expect(draft).to be_valid
-      end
-
-      it 'contentが140字のときバリデーションに成功すること' do
-        post.content = 'a' * 139
         expect(post).to be_valid
       end
     end
 
-    context "無効な属性のとき" do
-      it 'is_postedが真偽値以外の値ならバリデーションに失敗すること' do
+    context '存在しないとき' do
+      it 'バリデーションに失敗すること' do
         post.is_posted = nil
-        expect(post).to_not be_valid
-      end
-
-      it '投稿済かつpost_atが存在しないときバリデーションに失敗すること' do
-        # nilは許容するので空白でテスト
-        post.post_at = ''
-        expect(post).to_not be_valid
-      end
-
-      it 'contentが存在しなければバリデーションに失敗すること' do
-        post.content = ''
-        expect(post).to_not be_valid
-      end
-
-      it 'contentが140字以上ならバリデーションに失敗すること' do
-        post.content = 'a' * 141
-        expect(post).to_not be_valid
+        expect(post).to be_invalid
       end
     end
   end
 
-  describe "関連付けのテスト" do
+  describe 'attribute: content' do
+    context '存在するとき' do
+      it 'バリデーションに成功すること' do
+        expect(post).to be_valid
+      end
+    end
+
+    context '存在しないとき' do
+      it 'バリデーションに失敗すること' do
+        post.content = nil
+        expect(post).to be_invalid
+      end
+    end
+
+    context '空白のとき' do
+      it 'バリデーションに失敗すること' do
+        post.content = '  '
+        expect(post).to be_invalid
+      end
+    end
+
+    context '140字のとき' do
+      it 'バリデーションに成功すること' do
+        post.content = 'a' * 140
+        expect(post).to be_valid
+      end
+    end
+
+    context '141字のとき' do
+      it 'バリデーションに失敗すること' do
+        post.content = 'a' * 141
+        expect(post).to be_invalid
+      end
+    end
+  end
+
+  describe 'attribute: post_at' do
+    context '存在するとき' do
+      it 'バリデーションに成功すること' do
+        expect(post).to be_valid
+      end
+    end
+
+    context '下書きかつ存在しないとき' do
+      it 'バリデーションに成功すること' do
+        draft.post_at = nil
+        expect(draft).to be_valid
+      end
+    end
+
+    context '投稿済みかつ存在しないとき' do
+      it 'バリデーションに失敗すること' do
+        post.post_at = nil
+        expect(post).to be_invalid
+      end
+    end
+  end
+
+  describe "belongs_to: User" do
     let!(:user_with_post) { FactoryBot.create(:user, :with_post) }
 
     it 'Userが削除されればそれに伴って削除されること' do
