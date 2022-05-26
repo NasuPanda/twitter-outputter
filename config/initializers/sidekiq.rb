d56e464d0b663776d701d7b3b3b11f0cd7b57223
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-26 22:15:33 +0900

    fix: 本番環境用のRedis接続設定を追加

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,14 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379' }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379' }
-end
-
 require 'sidekiq/web'
 
 # NOTE: Sidekiqのwebuiにベーシック認証をかける
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == [ENV['BASIC_AUTH_USER'], ENV['BASIC_AUTH_PASSWORD']]
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379') }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379')}
 end
