266dfc4aafc3b77572bc4cf37909812898e5c003
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-26 21:57:22 +0900

    config: Sidekiqの設定を追加

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,10 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://localhost:6379' }
 end
+
+require 'sidekiq/web'
+
+# NOTE: Sidekiqのwebuiにベーシック認証をかける
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  [user, password] == [ENV['BASIC_AUTH_USER'], ENV['BASIC_AUTH_PASSWORD']]
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  # sidekiqのダッシュボード
+  mount Sidekiq::Web, at: '/sidekiq'
+
   root 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/auth/failure' => 'sessions#failure'
