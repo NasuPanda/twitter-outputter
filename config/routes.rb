13321002c00a6ee3ad5aad783fbd27e011ac1158
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-27 21:48:47 +0900

    fix: Tag→Tagsへ改名

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/auth/failure' => 'sessions#failure'
   delete '/logout' => 'sessions#destroy'
 
+  resources :tags
+
   # FIXME twitter controller 自体移す
   get '/tweet' => 'twitter#update'
   # FIXME 消す
