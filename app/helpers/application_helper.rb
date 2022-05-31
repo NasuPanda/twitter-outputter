52418416dcc8c8319813edf37810e94c3b164e80
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-31 21:38:23 +0900

    feat: title用ヘルパーの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # 完全なタイトルを返す
+  def full_title(page_title='')
+    base_title = 'Twitter Outputter'
+    if page_title.empty?
+      return base_title
+    else
+      return "#{page_title} - #{base_title}"
+    end
+  end
 end
