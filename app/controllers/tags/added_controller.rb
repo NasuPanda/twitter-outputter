6f5294f24a092e0d790dc1e916d094017ac7c5ef
Westen0511 <xiyuanjunzhi@gmail.com>
2022-06-05 14:29:02 +0900

    feat: AddedTagの描画を永続化

--- app/controllers/tags/added_controller.rb
+++ app/controllers/tags/added_controller.rb
@@ -1,9 +1,20 @@
 class Tags::AddedController < ApplicationController
   def create
-    # TODO sessionか何かでおぼえておくようにする
-    @tag_name = params[:tag_name]
+    tag = current_user.tags.find(tag_params[:tag_id])
+    # 既にaddedの場合は失敗する
+    if tag.is_added?
+      @status = '既に使用されているタグです。同じタグを使用することは出来ません。'
+      return
+    end
+    @status =  tag.update(is_added: true) ? 'success' : 'failure'
+    @tag = tag
   end
 
   def destroy
   end
+
+  private
+    def tag_params
+      params.permit(:tag_id)
+    end
 end
