cc3f308de3d4fdbd10a0f433f2e624e95d69669a
Westen0511 <xiyuanjunzhi@gmail.com>
2022-06-09 08:07:13 +0900

    feat: Post/Published#index, createの追加

--- app/controllers/posts/published_controller.rb
+++ app/controllers/posts/published_controller.rb
@@ -5,7 +5,26 @@ class Posts::PublishedController < ApplicationController
   end
 
   def create
-    # TODO 投稿する処理
-    # current_user.post_tweet
+    @post = current_user.posts.build(post_params)
+    # NOTE: 新規作成の場合, タグ付けされたタグをcontentに追加しておく
+    @post.add_tags_to_content(current_user.tagged_tags)
+    if @post.save
+      respond_to do |format|
+        format.html { redirect_to root_url, notice: '投稿に成功しました' }
+        format.js { @status = 'success' }
+        # current_user.post_tweet(@post.content)
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to root_url, alert: '投稿に失敗しました' }
+        format.js { @status = 'failure' }
+      end
+    end
   end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
 end
