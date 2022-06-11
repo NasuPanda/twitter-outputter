e8c0234f77f0846ee083781e0c1352bd4d334f4e
Westen0511 <xiyuanjunzhi@gmail.com>
2022-06-11 15:36:15 +0900

    feat: Post/Draft#destroy(to published/scheduledで分岐する)を追加

--- app/controllers/posts/drafts_controller.rb
+++ app/controllers/posts/drafts_controller.rb
@@ -5,10 +5,6 @@ class Posts::DraftsController < ApplicationController
       page(params[:page]).per(10)
   end
 
-  def new
-    @draft = current_user.posts.build
-  end
-
   # NOTE: status はデフォルトで draftのため指定不要
   def create
     @draft = current_user.posts.build(draft_params)
@@ -49,30 +45,34 @@ class Posts::DraftsController < ApplicationController
   # NOTE : 下書きから削除 → 予約投稿 or 投稿済 へ
   def destroy
     @draft = current_user.drafts.find(params[:id])
-    # TODO 予約投稿へ移動する分岐を追加する privateメソッドで分割すると良さげ
-    # ここ予約 or 投稿済 の振り分けロジックを書くとPostControllerと重複が発生するので, Post.createを呼び出す形でも良さそう
-    # if @draft.post_at
-      # @draft.status = :reserved
 
-    # 投稿済へ移動する
-    self.to_published
+    # params[:to]に渡した値で分岐する
+    if params[:to] == 'published'
+      self.to_published!
+    elsif params[:to] == 'scheduled'
+      self.to_scheduled!
+    else
+      # TODO エラー投げる
+    end
   end
 
   private
 
     def draft_params
-      params.require(:post).permit(:content)
+      params.require(:post).permit(:content, :post_at)
     end
 
-    # 投稿済に変更する
-    def to_published
+    # TODO sendメソッドでDRYにできそう
+    # 下書きを投稿する(インスタンス変数を直接操作する)
+    def to_published!
       @draft.to_published
+
       if @draft.save
         respond_to do |format|
           format.html { redirect_to root_url, notice: '投稿に成功しました' }
           format.js do
             @error_messages = []
-            @action = 'published'
+            @to = 'published'
           end
         end
       else
@@ -80,7 +80,36 @@ class Posts::DraftsController < ApplicationController
           format.html { redirect_to root_url, alert: '投稿に失敗しました' }
           format.js do
             @error_messages = error_messages_with_prefix(@draft, '投稿に失敗しました。')
-            @action = 'published'
+            @to = 'published'
+          end
+        end
+      end
+    end
+
+    # 下書きを予約投稿へ(インスタンス変数を直接操作する)
+    def to_scheduled!
+      # NOTE: post_atが登録されていなければeditに飛ばす
+      unless @draft.post_at
+        @error_messages = ['予約投稿に失敗しました。', '予約投稿するには投稿日時を設定してください。']
+        render :edit, content_type: 'text/javascript' and return
+      end
+
+      @draft.to_scheduled
+
+      if @draft.save
+        respond_to do |format|
+          format.html { redirect_to drafts_url, notice: '予約投稿に成功しました' }
+          format.js do
+            @error_messages = []
+            @to = 'scheduled'
+          end
+        end
+      else
+        respond_to do |format|
+          format.html { redirect_to drafts_url, alert: '予約投稿に失敗しました' }
+          format.js do
+            @error_messages = error_messages_with_prefix(@draft, '予約投稿に失敗しました。')
+            @to = 'scheduled'
           end
         end
       end
