f067847841fcee1151cd51425277f38d214b0fe8
Westen0511 <xiyuanjunzhi@gmail.com>
2022-06-12 17:09:48 +0900

    test: Postモデルの単体テストを追加

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,17 +1,21 @@
 FactoryBot.define do
   factory :post do
     sequence(:content) { |i| "content #{i}" }
-    status { 'published' }
-    post_at { rand(1..30).days.ago }
     association :user
 
     trait :draft do
       status { 'draft' }
+      post_at { nil }
     end
 
     trait :scheduled do
       status { 'scheduled' }
       post_at { rand(1..30).days.from_now }
+    end
+
+    trait :published do
+      status { 'published' }
+      post_at { rand(1..30).days.ago }
     end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -68,17 +68,17 @@ RSpec.describe Post, type: :model do
   end
 
   describe 'attribute: post_at' do
-    let(:post) { FactoryBot.build(:post) }
     let(:draft) { FactoryBot.build(:post, :draft) }
     let(:scheduled) { FactoryBot.build(:post, :scheduled) }
+    let(:published_post) { FactoryBot.build(:post, :published) }
 
-    context '存在するとき' do
-      it 'バリデーションに成功すること' do
-        expect(post).to be_valid
+    context '下書きの場合' do
+      context '存在するとき' do
+        it 'バリデーションに成功すること' do
+          expect(draft).to be_valid
+        end
       end
-    end
 
-    context '下書き' do
       context '存在しないとき' do
         it 'バリデーションに成功すること' do
           draft.post_at = nil
@@ -87,7 +87,7 @@ RSpec.describe Post, type: :model do
       end
     end
 
-    context '予約投稿' do
+    context '予約投稿の場合' do
       context '存在しないとき' do
         it 'バリデーションに失敗すること' do
           scheduled.post_at = nil
@@ -124,11 +124,17 @@ RSpec.describe Post, type: :model do
       end
     end
 
-    context '投稿済' do
+    context '投稿済の場合' do
+      context '存在するとき' do
+        it 'バリデーションに成功すること' do
+          expect(published_post).to be_valid
+        end
+      end
+
       context '存在しないとき' do
         it 'バリデーションに失敗すること' do
-          post.post_at = nil
-          expect(post).to be_invalid
+          published_post.post_at = nil
+          expect(published_post).to be_invalid
         end
       end
     end
@@ -138,8 +144,8 @@ RSpec.describe Post, type: :model do
     let(:user) { FactoryBot.create(:user) }
 
     it 'デフォルト値がdraftであること' do
-      created_post = user.posts.create(content: 'test content')
-      expect(created_post.draft?).to be_truthy
+      post = user.posts.build(content: 'test content')
+      expect(post.draft?).to be_truthy
     end
   end
 
@@ -153,15 +159,102 @@ RSpec.describe Post, type: :model do
     end
   end
 
-  # TODO Post ユニットテスト実装
+  describe '#to_draft' do
+    let(:scheduled_post) { FactoryBot.build(:post, :scheduled) }
+    let(:published_post) { FactoryBot.build(:post, :published) }
+
+    context '予約投稿のとき' do
+      it '下書きに変更されること' do
+        expect { scheduled_post.to_draft }.to change{ scheduled_post.status }
+          .from('scheduled').to('draft')
+      end
+    end
+
+    context '投稿済のとき' do
+      it 'PublishedToDraftError が発生すること' do
+        expect{ published_post.to_draft }.to raise_error(Post::PublishedToDraftError)
+      end
+    end
+  end
+
   describe '#to_published' do
+    let(:draft) { FactoryBot.build(:post, :draft) }
+    let(:scheduled_post) { FactoryBot.build(:post, :scheduled) }
+
+    before do
+      travel_to(Time.current)
+    end
+
+    context '下書きのとき' do
+      it '投稿済に変更されること' do
+        expect { draft.to_published }.to change{ draft.status }
+          .from('draft').to('published')
+      end
+
+      it 'post_atが現在時刻に変更されること' do
+        draft.to_published
+        expect(draft.post_at).to eq(Time.current)
+      end
+    end
+
+    context '予約投稿のとき' do
+      it '投稿済に変更されること' do
+        expect { scheduled_post.to_published }.to change{ scheduled_post.status }
+          .from('scheduled').to('published')
+      end
+
+      it 'post_atが現在時刻に変更されること' do
+        scheduled_post.to_published
+        expect(scheduled_post.post_at).to eq(Time.current)
+      end
+    end
   end
 
-  # TODO Post ユニットテスト実装
   describe '#to_scheduled' do
+    let(:draft) { FactoryBot.build(:post, :draft) }
+    let(:published_post) { FactoryBot.build(:post, :published) }
+
+    context '下書きのとき' do
+      it '予約投稿に変更されること' do
+        expect { draft.to_scheduled }.to change{ draft.status }
+          .from('draft').to('scheduled')
+      end
+    end
+
+    context '投稿済のとき' do
+      it 'PublishedToScheduledError が発生すること' do
+        expect{ published_post.to_scheduled }.to raise_error(Post::PublishedToScheduledError)
+      end
+    end
   end
 
-  # TODO Post ユニットテスト実装
   describe '#add_tags_to_content' do
+    let(:post) { FactoryBot.build(:post) }
+    let(:tags) { FactoryBot.create_list(:tag, 3) }
+
+    context 'タグのリストが渡されたとき' do
+      it 'post.contentに半角スペース区切りでtag.nameが追加されること' do
+        expected_content = post.content.dup
+        tags.each do |tag|
+          expected_content += ' '
+          expected_content += tag.name
+        end
+
+        post.add_tags_to_content(tags)
+        expect(post.content).to eq(expected_content)
+      end
+    end
+
+    context '空のリストが渡されたとき' do
+      it 'post.contentが変化しないこと' do
+        expect{ post.add_tags_to_content([]) }.to_not change{ post.content }
+      end
+    end
+
+    context 'タグ以外のリストが渡されたとき' do
+      it 'NoMethodError が発生すること' do
+        expect{ post.add_tags_to_content(['tag', 'tag2']) }.to raise_error(NoMethodError)
+      end
+    end
   end
 end
