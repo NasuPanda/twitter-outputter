cef81e8eda425b0783e41e1e5837598f3bb332c7
Westen0511 <xiyuanjunzhi@gmail.com>
2022-06-24 07:22:17 +0900

    fix: タイポの修正

--- spec/factories/notification_settings.rb
+++ spec/factories/notification_settings.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :notification_setting do
     can_notify { false }
-    notify_at { Time.now }
+    notify_at { Time.zone.local(2022, 4, 1, 0, 0, 0) }
     interval_to_check { rand(1..30) }
     association :user
 
--- spec/jobs/check_existence_of_post_job_spec.rb
+++ spec/jobs/check_existence_of_post_job_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe CheckExistenceOfPostJob, type: :job do
   describe '#perform' do
     let!(:user) { FactoryBot.create(:user) }
-    let!(:notification_setting) { FactoryBot.create(:notification_setting, notify_at: true, user: user) }
+    let!(:notification_setting) { FactoryBot.create(:notification_setting, can_notify: true, user: user) }
 
     it 'ジョブがキューに入ること' do
       expect {
@@ -16,7 +16,7 @@ RSpec.describe CheckExistenceOfPostJob, type: :job do
     describe 'ジョブ実行のテスト' do
       before do
         allow_any_instance_of(User).to receive(:most_recent_tweet).and_return('recent tweet')
-        allow_any_instance_of(CheckExistenceOfPostJob).to receive(:tweet_exist?).and_return(true)
+        allow_any_instance_of(CheckExistenceOfPostJob).to receive(:tweet_exist_in_period?).and_return(true)
       end
 
       it '指定時間にジョブが実行されること' do
@@ -38,14 +38,14 @@ RSpec.describe CheckExistenceOfPostJob, type: :job do
         CheckExistenceOfPostJob
         .set(wait_until: notification_setting.check_tweet_existence_time)
         .perform_later(user)
-      currently_queued_job = enqueued_jobs.first
+        currently_queued_job = enqueued_jobs.first
 
-      expect {
-        perform_enqueued_jobs(
-          only: ->(job) {
-            job['job_id'] == currently_queued_job['job_id'] }
-        )
-      }.to enqueue_job(CheckExistenceOfPostJob).on_queue(:check_post)
+        expect {
+          perform_enqueued_jobs(
+            only: ->(job) {
+              job['job_id'] == currently_queued_job['job_id'] }
+          )
+        }.to enqueue_job(CheckExistenceOfPostJob).on_queue(:check_post)
       end
 
       it 'perform後に追加されたジョブの実行時間がNotification_setting#check_tweet_existence_timeであること' do
