bd563c15138b881c3131ff0e3276774e3f83b682
Westen0511 <xiyuanjunzhi@gmail.com>
2022-06-26 22:27:34 +0900

    test: PostHelper#post_card_textsのテストを追加

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -1,6 +1,82 @@
 require 'rails_helper'
 
 RSpec.describe PostsHelper, type: :helper do
+  describe 'post_card_texts' do
+    let(:opening_tag) { helper.send(:card_text_p_tag)[0] }
+    let(:closing_tag) { helper.send(:card_text_p_tag)[1] }
+
+    context '改行を含まないとき' do
+      let(:text) { '改行コードを含まないテキスト' }
+
+      it '返り値の各要素が開始タグ, 閉じタグを含むこと' do
+        post = double('Post', content: text)
+        helper.post_card_texts(post).each do |card_text|
+          expect(card_text).to be_include(opening_tag)
+          expect(card_text).to be_include(closing_tag)
+        end
+      end
+
+      it '長さ1の配列が返ること' do
+        post = double('Post', content: text)
+        expect(helper.post_card_texts(post).size).to eq(1)
+      end
+    end
+
+    context '改行文字を3つ含むとき' do
+      let(:text) { "改行コードを\n3つ\n含む\nテキスト" }
+
+      it '返り値の各要素が開始タグ, 閉じタグを含むこと' do
+        post = double('Post', content: text)
+        helper.post_card_texts(post).each do |card_text|
+          expect(card_text).to be_include(opening_tag)
+          expect(card_text).to be_include(closing_tag)
+        end
+      end
+
+      it '長さ4の配列が返ること' do
+        post = double('Post', content: text)
+        expect(helper.post_card_texts(post).size).to eq(4)
+      end
+
+      it '返り値の各要素が正しいテキストを持つこと' do
+        post = double('Post', content: text)
+        first, second, third, fourth = helper.post_card_texts(post)
+        expect(first).to be_include('改行コードを')
+        expect(second).to be_include('3つ')
+        expect(third).to be_include('含む')
+        expect(fourth).to be_include('テキスト')
+      end
+    end
+
+    context '改行文字を5つ含むとき' do
+      let(:text) { "改行\nコード\nを\n5つ\n含む\nテキスト" }
+
+      it '返り値の各要素が開始タグ, 閉じタグを含むこと' do
+        post = double('Post', content: text)
+        helper.post_card_texts(post).each do |card_text|
+          expect(card_text).to be_include(opening_tag)
+          expect(card_text).to be_include(closing_tag)
+        end
+      end
+
+      it '長さ6の配列が返ること' do
+        post = double('Post', content: text)
+        expect(helper.post_card_texts(post).size).to eq(6)
+      end
+
+      it '返り値の各要素が正しいテキストを持つこと' do
+        post = double('Post', content: text)
+        first, second, third, fourth, fifth, sixth = helper.post_card_texts(post)
+        expect(first).to be_include('改行')
+        expect(second).to be_include('コード')
+        expect(third).to be_include('を')
+        expect(fourth).to be_include('5つ')
+        expect(fifth).to be_include('含む')
+        expect(sixth).to be_include('テキスト')
+      end
+    end
+  end
+
   describe 'post_id' do
     it 'post-#{post.id}を返すこと' do
       post = double('Post', id: 1)
