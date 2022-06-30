0889445d1731bbd944ef7de2a7078ba8e3773710
Westen0511 <xiyuanjunzhi@gmail.com>
2022-06-30 20:32:15 +0900

    test: Posts/Images#destroyのテストを追加

--- spec/requests/posts/images_spec.rb
+++ spec/requests/posts/images_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Posts::Images", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'DELETE posts/images/:id' do
+    context 'ログインユーザのとき' do
+      let!(:user) { FactoryBot.create(:user, :with_authentication) }
+      let!(:post_with_image) { FactoryBot.create(:post, :with_image, user: user) }
+      let!(:image) { post_with_image.images.first }
+
+      before { sign_in_as(user) }
+
+      it '画像を削除出来ること' do
+        expect{
+          delete posts_image_path(image, post_id: post_with_image.id), xhr: true
+        }.to change{ post_with_image.images.count }.by(-1)
+      end
+    end
+
+    context 'ログインしていないユーザのとき' do
+      let!(:post_with_image) { FactoryBot.create(:post, :with_image) }
+      let!(:image) { post_with_image.images.first }
+
+      it '画像を削除出来ないこと' do
+        expect{
+          delete posts_image_path(image, post_id: post_with_image.id), xhr: true
+        }.to_not change{ post_with_image.images.count }
+      end
+
+      it 'rootにリダイレクトされること' do
+        delete posts_image_path(image, post_id: post_with_image.id), xhr: true
+        expect(response).to redirect_to(root_url)
+      end
+    end
   end
 end
